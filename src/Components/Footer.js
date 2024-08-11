637827b395953ebde7d15bdcf1b5d318dfc36eff
vishu-bhandari <deevishu1234@gmail.com>
2024-08-12 05:14:57 +0530

    Login added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -179,12 +179,20 @@ function Footer() {
             </p>
             <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
               <li>
-                <a
+                {/* <a
                   href="/"
                   className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
                 >
                   F.A.Q
-                </a>
+                </a> */}
+               
+                <Link
+                  to="/AdminLogin"
+                  className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                  Log in
+                </Link>
+             
               </li>
               <li>
                 <a
